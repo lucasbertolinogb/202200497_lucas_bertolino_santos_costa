1d4ef2a73b1fd1a5755f8584b15588ba45e1d0d0
lucasbertolinogb <102763228+lucasbertolinogb@users.noreply.github.com>
2023-05-18 19:12:00 -0300

    Create hello.py

---
+++ pset-6/hello.py
@@ -0,0 +1,10 @@
+# TODO
+# vai perguntar seu nome
+print("What is your name? ")
+
+# vai pegar a variável nome que o
+# usuário digitar
+nome = input("")
+
+# vai aparecer "hello, + nome(escrito em cima)"
+print("hello, " + nome)
